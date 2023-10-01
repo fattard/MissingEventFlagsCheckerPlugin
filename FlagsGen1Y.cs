63c2de83a6c10ed716223a30395897d4ae335092
fattard <fattard@users.noreply.github.com>
2023-10-01 17:07:25 -0300

    Updated flags list with inclusion of Badge flags. Preparing code to work with other save language offsets

--- FlagsGen1Y.cs
+++ FlagsGen1Y.cs
@@ -11,34 +11,69 @@ namespace MissingEventFlagsCheckerPlugin
     {
         static string s_flagsList_res = null;
 
-        enum FlagOffsets
+        enum FlagOffsets_INTL
         {
-            MissableObjectFlags = 0x2852,
-            ObtainedHiddenItems = 0x299C,
-            ObtainedHiddenCoins = 0x29AA,
-            RodFlags = 0x29D4,
-            LaprasFlag = 0x29DA,
-            CompletedInGameTradeFlags = 0x29E3,
-            EventFlags = 0x29F3
+            BadgeFlags_INTL = 0x2602,
+            MissableObjectFlags_INTL = 0x2852,
+            ObtainedHiddenItems_INTL = 0x299C,
+            ObtainedHiddenCoins_INTL = 0x29AA,
+            RodFlags_INTL = 0x29D4,
+            LaprasFlag_INTL = 0x29DA,
+            CompletedInGameTradeFlags_INTL = 0x29E3,
+            EventFlags_INTL = 0x29F3,
         }
+
+        int BadgeFlagsOffset;
+        int MissableObjectFlagsOffset;
+        int ObtainedHiddenItemsOffset;
+        int ObtainedHiddenCoinsOffset;
+        int RodFlagsOffset;
+        int LaprasFlagOffset;
+        int CompletedInGameTradeFlagsOffset;
+        int EventFlagsOffset;
 
         const int Src_EventFlags = 0;
         const int Src_HideShowFlags = 1;
         const int Src_HiddenItemFlags = 2;
         const int Src_HiddenCoinsFlags = 3;
         const int Src_TradeFlags = 4;
-        const int Src_MiscFlags = 5;
+        const int Src_BadgesFlags = 5;
+        const int Src_MiscFlags = 6;
 
 
         protected override void InitFlagsData(SaveFile savFile)
         {
             m_savFile = savFile;
 
-            // wMissableObjectIndex
-            bool[] result = new bool[32 * 8];
+            if ((m_savFile as SAV1).Japanese)
+            {
+                //TODO:
+            }
+            else
+            {
+                BadgeFlagsOffset = (int)FlagOffsets_INTL.BadgeFlags_INTL;
+                MissableObjectFlagsOffset = (int)FlagOffsets_INTL.MissableObjectFlags_INTL;
+                ObtainedHiddenItemsOffset = (int)FlagOffsets_INTL.ObtainedHiddenItems_INTL;
+                ObtainedHiddenCoinsOffset = (int)FlagOffsets_INTL.ObtainedHiddenCoins_INTL;
+                RodFlagsOffset = (int)FlagOffsets_INTL.RodFlags_INTL;
+                LaprasFlagOffset = (int)FlagOffsets_INTL.LaprasFlag_INTL;
+                CompletedInGameTradeFlagsOffset = (int)FlagOffsets_INTL.CompletedInGameTradeFlags_INTL;
+                EventFlagsOffset = (int)FlagOffsets_INTL.EventFlags_INTL;
+            }
+
+            // wObtainedBadges
+            bool[] result = new bool[8];
             for (int i = 0; i < result.Length; i++)
             {
-                result[i] = m_savFile.GetFlag((int)FlagOffsets.MissableObjectFlags + (i >> 3), i & 7);
+                result[i] = m_savFile.GetFlag(BadgeFlagsOffset + (i >> 3), i & 7);
+            }
+            bool[] badgeFlags = result;
+
+            // wMissableObjectIndex
+            result = new bool[32 * 8];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = m_savFile.GetFlag(MissableObjectFlagsOffset + (i >> 3), i & 7);
             }
             bool[] missableObjectFlags = result;
 
@@ -46,7 +81,7 @@ namespace MissingEventFlagsCheckerPlugin
             result = new bool[112];
             for (int i = 0; i < result.Length; i++)
             {
-                result[i] = m_savFile.GetFlag((int)FlagOffsets.ObtainedHiddenItems + (i >> 3), i & 7);
+                result[i] = m_savFile.GetFlag(ObtainedHiddenItemsOffset + (i >> 3), i & 7);
             }
             bool[] obtainedHiddenItemsFlags = result;
 
@@ -54,7 +89,7 @@ namespace MissingEventFlagsCheckerPlugin
             result = new bool[16];
             for (int i = 0; i < result.Length; i++)
             {
-                result[i] = m_savFile.GetFlag((int)FlagOffsets.ObtainedHiddenCoins + (i >> 3), i & 7);
+                result[i] = m_savFile.GetFlag(ObtainedHiddenCoinsOffset + (i >> 3), i & 7);
             }
             bool[] obtainedHiddenCoinsFlags = result;
 
@@ -62,17 +97,17 @@ namespace MissingEventFlagsCheckerPlugin
             result = new bool[16];
             for (int i = 0; i < result.Length; i++)
             {
-                result[i] = m_savFile.GetFlag((int)FlagOffsets.CompletedInGameTradeFlags + (i >> 3), i & 7);
+                result[i] = m_savFile.GetFlag(CompletedInGameTradeFlagsOffset + (i >> 3), i & 7);
             }
             bool[] completedInGameTradeFlags = result;
 
             // wd728
-            bool gotOldRod = m_savFile.GetFlag((int)FlagOffsets.RodFlags, 3);
-            bool gotGoodRod = m_savFile.GetFlag((int)FlagOffsets.RodFlags, 4);
-            bool gotSuperRod = m_savFile.GetFlag((int)FlagOffsets.RodFlags, 5);
+            bool gotOldRod = m_savFile.GetFlag(RodFlagsOffset, 3);
+            bool gotGoodRod = m_savFile.GetFlag(RodFlagsOffset, 4);
+            bool gotSuperRod = m_savFile.GetFlag(RodFlagsOffset, 5);
 
             // wd72e
-            bool gotLaprasFlag = m_savFile.GetFlag((int)FlagOffsets.LaprasFlag, 0);
+            bool gotLaprasFlag = m_savFile.GetFlag(LaprasFlagOffset, 0);
 
             // Unified Misc flags
             result = new bool[] { gotLaprasFlag, gotOldRod, gotGoodRod, gotSuperRod };
@@ -96,6 +131,7 @@ namespace MissingEventFlagsCheckerPlugin
             int idxHiddenItemsSection = s_flagsList_res.IndexOf("//\tHidden Items Flags");
             int idxHiddenCoinsSection = s_flagsList_res.IndexOf("//\tHidden Coins Flags");
             int idxTradesSection = s_flagsList_res.IndexOf("//\tIn-Game Trades Flags");
+            int idxBadgesSection = s_flagsList_res.IndexOf("//\tBadges Flags");
             int idxMiscSection = s_flagsList_res.IndexOf("//\tMisc");
 
             m_eventFlagsList.Clear();
@@ -105,6 +141,7 @@ namespace MissingEventFlagsCheckerPlugin
             AssembleList(s_flagsList_res.Substring(idxHiddenItemsSection), Src_HiddenItemFlags, obtainedHiddenItemsFlags);
             AssembleList(s_flagsList_res.Substring(idxHiddenCoinsSection), Src_HiddenCoinsFlags, obtainedHiddenCoinsFlags);
             AssembleList(s_flagsList_res.Substring(idxTradesSection), Src_TradeFlags, completedInGameTradeFlags);
+            AssembleList(s_flagsList_res.Substring(idxBadgesSection), Src_BadgesFlags, badgeFlags);
             AssembleList(s_flagsList_res.Substring(idxMiscSection), Src_MiscFlags, miscFlags);
 
             // Reset S.S. Anne
@@ -161,19 +198,23 @@ namespace MissingEventFlagsCheckerPlugin
                             break;
 
                         case Src_HideShowFlags:
-                            m_savFile.SetFlag((int)FlagOffsets.MissableObjectFlags + (fIdx >> 3), fIdx & 7, value);
+                            m_savFile.SetFlag(MissableObjectFlagsOffset + (fIdx >> 3), fIdx & 7, value);
                             break;
 
                         case Src_HiddenItemFlags:
-                            m_savFile.SetFlag((int)FlagOffsets.ObtainedHiddenItems + (fIdx >> 3), fIdx & 7, value);
+                            m_savFile.SetFlag(ObtainedHiddenItemsOffset + (fIdx >> 3), fIdx & 7, value);
                             break;
 
                         case Src_HiddenCoinsFlags:
-                            m_savFile.SetFlag((int)FlagOffsets.ObtainedHiddenCoins + (fIdx >> 3), fIdx & 7, value);
+                            m_savFile.SetFlag(ObtainedHiddenCoinsOffset + (fIdx >> 3), fIdx & 7, value);
                             break;
 
                         case Src_TradeFlags:
-                            m_savFile.SetFlag((int)FlagOffsets.CompletedInGameTradeFlags + (fIdx >> 3), fIdx & 7, value);
+                            m_savFile.SetFlag(CompletedInGameTradeFlagsOffset + (fIdx >> 3), fIdx & 7, value);
+                            break;
+
+                        case Src_BadgesFlags:
+                            m_savFile.SetFlag(BadgeFlagsOffset + (fIdx >> 3), fIdx & 7, value);
                             break;
 
                         case Src_MiscFlags:
@@ -181,19 +222,19 @@ namespace MissingEventFlagsCheckerPlugin
                                 switch (fIdx)
                                 {
                                     case 0x00: // Lapras flag
-                                        m_savFile.SetFlag((int)FlagOffsets.LaprasFlag, 0, value);
+                                        m_savFile.SetFlag(LaprasFlagOffset, 0, value);
                                         break;
 
                                     case 0x01: // Old Rod flag
-                                        m_savFile.SetFlag((int)FlagOffsets.RodFlags, 3, value);
+                                        m_savFile.SetFlag(RodFlagsOffset, 3, value);
                                         break;
 
                                     case 0x02: // Good Rod flag
-                                        m_savFile.SetFlag((int)FlagOffsets.RodFlags, 4, value);
+                                        m_savFile.SetFlag(RodFlagsOffset, 4, value);
                                         break;
 
                                     case 0x03: // Super Rod flag
-                                        m_savFile.SetFlag((int)FlagOffsets.RodFlags, 5, value);
+                                        m_savFile.SetFlag(RodFlagsOffset, 5, value);
                                         break;
                                 }
                             }
