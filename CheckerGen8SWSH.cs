80cf379c6fa88348b407b1953a8db42072aa7773
fattard <fattard@users.noreply.github.com>
2024-11-24 21:32:08 -0300

    Updated SwSh field items and hidden items

--- CheckerGen8SWSH.cs
+++ CheckerGen8SWSH.cs
@@ -16,6 +16,7 @@
             s_chkdb_res ??= ReadResFile("chkdb_gen8swsh");
 
             m_flagsSourceInfo["0"] = 0;
+            m_flagsSourceInfo["1"] = 1;
             m_flagsSourceInfo["-"] = -1;
 
             ParseChecklist(s_chkdb_res);
@@ -31,6 +32,38 @@
             {
                 case 0: // Bool blocks
                     isEvtSet = (savEventBlocks.GetBlockSafe((uint)idx).Type == SCTypeCode.Bool2);
+                    break;
+
+                case 1: // Hidden Items
+                    {
+                        // Hidden Item data
+                        // [0] - state 0: active
+                        //             1: active (respawned)
+                        //             2: obtained (will recycle)
+                        //             3: obtained (permanently)
+
+                        if (idx < 512)
+                        {
+                            isEvtSet = savEventBlocks.GetBlockSafe(0x6148F6AC).Data[(int)idx * 4] >= 2;
+                        }
+                        else if (idx < 1024)
+                        {
+                            var data = savEventBlocks.GetBlockSafe(0xE479EE37).Data;
+                            if (data?.Length == 0x810)
+                            {
+                                isEvtSet = data[((int)idx - 512) * 4] >= 2;
+                            }
+                        }
+                        else if (idx < 1536)
+                        {
+                            var data = savEventBlocks.GetBlockSafe(0xE579EFCA).Data;
+                            if (data?.Length == 0x810)
+                            {
+
+                                isEvtSet = data[((int)idx - 1024) * 4] >= 2;
+                            }
+                        }
+                    }
                     break;
 
                 default:
